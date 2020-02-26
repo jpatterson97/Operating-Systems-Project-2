f092170368f89cc8b33e053861d2d7ee40da5ca3
jpatterson97 <40641096+jpatterson97@users.noreply.github.com>
2020-02-25 21:26:05 -0500

    Update memory_sim.c

--- memory_sim.c
+++ memory_sim.c
@@ -1,7 +1,24 @@
 #include "memory_sim.h"
 #include <stdio.h>
 
+struct frame{
+  dirtybit = 0;
+  nframes[]; //? 
+};
+
 int main(){
+    
+  #variables
+  unsigned addr;
+  char rw;
+  struct frame arr_framenum[/*"Input from user in make file"*/];
+  //file is one of the four trace files
+  fscanf(file,"%x %c", &addr, &rw);
   
+  
+    printf("Total Memory Frames: ",/* Same variable as user inputs*/ );
+    printf("Events In Trace: ",/* Number of line reads*/ );
+    printf("Total Disk Reads: ", );
+    printf("Total Disk Writes: ", );
     return 0;
 }
