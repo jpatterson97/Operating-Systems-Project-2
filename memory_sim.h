c4be3eea74fc9e1c1871b24f3026597b1e2fa1b0
jpatterson97 <40641096+jpatterson97@users.noreply.github.com>
2020-02-25 21:26:01 -0500

    Update memory_sim.h

--- memory_sim.h
+++ memory_sim.h
@@ -1,2 +1,19 @@
 #include <stdio.h>
 
+rdm(/* Frame number, addr, rw*/){
+  //randomly selects page to replace
+}
+
+lru(){
+  //least recently used
+}
+
+fifo(){
+  //first in first out
+}
+
+vms(){
+  //second chance page replacement policy
+}
+
+//each of these needs to also change the counters accordingly for the r and w
