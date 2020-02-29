2a2d8acc6a697cb10806a13b611ff1e4ca359a86
jpatterson97 <40641096+jpatterson97@users.noreply.github.com>
2020-02-29 15:32:45 -0500

    Update memory_sim.h

--- memory_sim.h
+++ memory_sim.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 struct Frame{
+	//I think we might also need rw for each as well?
 	bool dirtybit;
 	unsigned address;
 };
@@ -14,64 +15,77 @@ struct Frame page_table[nframes];
   //least recently used
 //}
 
+
+//first in first out
 void fifo(){
-	//first in first out
-	int readCount, writeCount, *p;
+	//variables	
+	int readcount, writecount, *p, found;
+	//pointer to the first element of frame array
 	*p = page_table;
+	//while loop goes through each line in the trace file
 	while(fscanf(file, "%x %c", &addr, &rw) != EOF)
 	{
+		//adds each line up
 		numLines++;
-		
+		//for loop goes through first to see if it finds the value already in the frames
 		for(int i=0; i < nframes; i++){
+			//if it finds that address already in the frames
 			if(page_table[i].address == addr){
+				//it will check if it had a W (on original line in trace file)
 				if(rw=='W"){
-				   	dirtybit=1;
-				   found =1;	
+				   //make the bit dirty
+				   dirtybit=1;
+				   //and change it to found
+				   found =1;
+				   //break out of this for loop
 				   break;
 				}
 			}
 		}
-			if (found==0){
-				for(int i=0; i < nframes; i++){
-					if(page_table[i] == 0){
-						page_table[i].address = addr;
-						readcount++;
-						if(rw =='W')
-							dirtybit=1;
-							break;
-					else{
-						go to where pointer is
-						if (dirtybit == 1)
-							writecount ++;
-						readcount ++;
-						if (pointer Is at last frame)
-							p++;
-						else
-							p goes back to beginning (page_table[0]
-										  
+		//says if its not found in the frames (which this should be skipped if it went through that first loop and did find it
+		if (found==0){
+			//loop will go through frames again
+			for(int i=0; i < nframes; i++){
+				//checks if the frame is empty
+				if(page_table[i] == 0){
+					//if it is then put the address in first empty frame found
+					page_table[i].address = addr;
+					//increase readcount
+					readcount++;
+					//check if the address is a W
+					if(rw =='W'){
+						//change bit to dirty
+						dirtybit=1;
+						//break out of this for loop
+						break;
 					}
+				//if all of them are full
+				else{
+					//go to where pointer is (in beginning it should be first frame)
+					go to where pointer is
+					//if dirty bit of that frame is dirty add to write counter
+					if (dirtybit == 1){
+						writecount ++;
+					}
+					//add to read counter
+					readcount ++;
+					//if pointer is not at the last frame move pointer to next frame 
+					if (pointer is not at the last frame){
+						p++;
+					}
+					//if pointer is at last frame move it back to beginning
+					else{
+						p goes back to beginning (page_table[0])
+					}			  
 				}
-				
 			}
 		}
-		
-		/*for(int i=0; i < nframes; i++){
-			if(page_table[i] == 0){
-				page_table[i].address = addr;
-				//break;
-			}
-			else{
-				
-			}
-		}*/
-		
-		
+		//reset found back to 0 for the next address
+		found =0;
 	}
 }
 
 //void vms(){
   //second chance page replacement policy
 //}
-
-//each of these needs to also change the counters accordingly for the r and w
 
