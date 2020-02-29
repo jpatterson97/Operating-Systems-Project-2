09c91785df8b6f18386eddc0c0d7e893464931f6
jpatterson97 <40641096+jpatterson97@users.noreply.github.com>
2020-02-29 11:00:37 -0500

    Add files via upload

--- memory_sim.h
+++ memory_sim.h
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
+struct Frame{
+	bool dirtybit;
+	unsigned addr;
+};
+
 //rdm(/* Frame number, addr, rw*/){
   //randomly selects page to replace
 //}
-/*
-lru(){
+
+//lru(){
   //least recently used
-}
+//}
 
 fifo(){
-  //first in first out
+	//first in first out
+	while(fscanf(file, "%x %c", &addr, &rw) != EOF)
+	{
+		numLines++;
+		if(rw == 'W'){
+			dirtybit=1;	
+		}
+		
+	}
 }
 
-vms(){
+//vms(){
   //second chance page replacement policy
-}
+//}
 
 //each of these needs to also change the counters accordingly for the r and w
-*/
+
